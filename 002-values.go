02476e0adea5a8448cfd63ca6536c36725ad2234
Mustafa Gönül <mustafagonul@gmail.com>
2018-10-07 21:44:20 +0300

    - 002-values.go

---
+++ 002-values.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	// String
+	fmt.Println("Mustafa " + "Gonul")
+
+	// Integer  & Float
+	fmt.Println("1 / 2     = ", 1/2)
+	fmt.Println("7.0 / 2   = ", 7.0/2)
+	fmt.Println("8.0 / 3.0 = ", 8.0/3.0)
+
+	// Boolean
+	fmt.Println(true)
+	fmt.Println(false)
+}
